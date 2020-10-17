642249559a8dad9055ebd107143ad2f04d1a7ab6
zagcha <64097681+zagcha@users.noreply.github.com>
2020-10-18 01:45:44 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,125 @@
+$(`${'<img style="margin-left: -1px;width:101%; margin-top: 0px;pointer-events:none;margin-bottom: 0px;" src="https://g.top4top.io/p_1713a2b631.png " > <a id="jfa44"target="_blank"type="a"rel="nofollow"href="https://www.dookmobile.com"class="label-primary">Dook Mobile</a></div></div><center>\x0D\x0A\x0D\x0A<a id="asim99" href=""><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a></div></div>\x0D\x0A<div id="asim1"><a id="asim2"target="_blank"type="a"href="rules.html"style="border-radius: 0 10% 0 0; border-right: 2px solid#a6a6a6;"class="btn minix btn-primary">\u0627\u0644\u0642\u0648\u0627\u0646\u064A\u0646</a><a id="asim2"target="_blank"type="a"href="sub.html"style="border-radius: 0%; border-right: 1px solid#f5f8fa;"class="btn minix btn-primary">\u0627\u0644\u0627\u0634\u062A\u0631\u0627\u0643\u0627\u062A</a><a id="asim2"target="_blank"type="a"href="https://iraqia-chat.com/"style="border-radius: 0%; border-right: 1px solid#f5f8fa;"class="btn minix btn-primary">\u0634\u0627\u062A \u062C\u0641\u0649</a><a id="asim10"target="_blank"type="a"href="contact.html"style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa"class="btn minix btn-primary">\u0627\u062A\u0635\u0644 \u0628\u0646\u0627</a><a id="asim10"target="_blank"type="a"href="ruless.html"style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">\u0627\u0644\u0631\u0627\u062F\u064A\u0648</a><a id="asim10"target="_blank"type="a"href="mobile.html "style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">\u062A\u0637\u0628\u064A\u0642</a><a id="asim10"target="_blank"type="a"href="Shortcut.html"style="border-radius: 0 0 0 10%; border-left: 2px solid#a6a6a6; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">\u0627\u062E\u062A\u0635\u0627\u0631\u0627\u062A</a>\x0D\x0A<a id="hack2"target="_blank"type="a"href="https://chat.3asl.net/"style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa"class="btn minix btn-primary">\u0627\u0644\u0645\u062F\u0648\u0646\u0629</a><a id="hack2"target="_blank"type="a"href="https://www.abraj2015.com/"style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">\u0627\u0644\u0622\u0628\u0631\u0627\u062C</a><a id="hack2"target="_blank"type="a"href="https://www.wadhefa.com/"style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">\u0648\u0638\u0627\u0626\u0641</a>\x0D\x0A<a id="hack3"#="#"type="#"href="#"style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">عراقية شات</a>\x0D\x0A<center>'}`)['insertBefore']('.nav-tabs');
+$(`${'<a href="https://chat.3asl.net/" target="_blank" style=" font-family: \'El Messiri\', sans-serif;width:100px;color: #E8415D;text-decoration:none; margin-right: 3px;\x0D\x0Amargin-left: 2px;text-align: center;float: right;border-radius: 5px;\x0D\x0Aborder-color: #F2F2F2;\x0D\x0Apadding: 4.0px;" class="border" ><i class="fa fa-forumbee"></i>&nbsp;\u0627\u0644\u0645\u062F\u0648\u0646\u0629</a>'}`)['insertBefore']('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+$('.ae.label.label-primary.fa.fa-gear')['text'](' \u0636\u0628\u0637');
+$('.ae.fa.label.label-primary.fa-users')['text']('\u063A\u0631\u0641');
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%');
+$('.checkbox label')['html'](`${'<input id="stealth" type="checkbox" value=""><img id="as13" src="sico/z1ddjdmlvp10.png" width="70%" style="">'}`);
+$('div#tlogins button.btn.btn-primary')['click'](function() {
+    var _0xccffx1 = setInterval(function() {
+        var _0xccffx2 = $('.pmsgc')['length'];
+        0 < _0xccffx2 ? ($(`${' </div><center>\x0D\x0A'}`)['appendTo']('#mic'), clearInterval(_0xccffx1)) : console['log'](_0xccffx2)
+    }, 1)
+}), $('div#tlogins button.btn.btn-primary')['click'](function() {
+    setTimeout(function() {
+        refreshonline()
+    }, 0)
+}), setInterval(function() {
+    var _0xccffx3 = $('#dpnl');
+    0 < _0xccffx3['length'] && _0xccffx3['remove']()
+}, 0), $('.dpnl')['append'](`${' <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><center></div><center></div>'}`), eval(function(_0xccffx4, _0xccffx3, _0xccffx2, _0xccffx5, _0xccffx6, _0xccffx7) {
+    _0xccffx6 = function(_0xccffx2) {
+        return (_0xccffx2 < _0xccffx3 ? '' : _0xccffx6(parseInt(_0xccffx2 / _0xccffx3))) + ((_0xccffx2 = _0xccffx2 % _0xccffx3) > 35 ? String['fromCharCode'](_0xccffx2 + 29) : _0xccffx2.toString(36))
+    };
+    if (!'' ['replace'](/^/, String)) {
+        while (_0xccffx2--) {
+            _0xccffx7[_0xccffx6(_0xccffx2)] = _0xccffx5[_0xccffx2] || _0xccffx6(_0xccffx2)
+        };
+        _0xccffx5 = [function(_0xccffx6) {
+            return _0xccffx7[_0xccffx6]
+        }];
+        _0xccffx6 = function() {
+            return '\w+'
+        };
+        _0xccffx2 = 1
+    };
+    while (_0xccffx2--) {
+        if (_0xccffx5[_0xccffx2]) {
+            _0xccffx4 = _0xccffx4['replace'](new RegExp('\b' + _0xccffx6(_0xccffx2) + '\b', 'g'), _0xccffx5[_0xccffx2])
+        }
+    };
+    return _0xccffx4
+}('$(\'#b\').c(`<1 2="\3\4\5\d\e \f \4\5\3\g\h\i"j="#"k="$(\'.l\').m($(6).n(\'2\'));\x0Ao($(6),\'7\');\x0Ap(q()\x0A	{\x0A	$(\'#8\').r(0);\x0A}\x0A,0);\x0A$(\'.s\').t();\x0A"9-u="v"9-w="#8"x="y a 1 1-7 a-z-A">&B;</1>`);', 38, 38, '|label|title|u0645|u0627|u0644|this|primary|mic|data|fa|d0|append|u0643|u064a|u002d|u0648|u0642|u0639|href|onclick|pnhead|text|attr|hl|setTimeout|function|scrollTop|dpnl|show|toggle|tab|target|class|ae|volume|up|nbsp' ['split']('|'), 0, {}));
+$(`${'<div><marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;أهلا وسهلا بكم في شات <font color="#595757" class="Kaz99">♥ || عراقيــة لـلـجــوال || ♥</font> ● · ‫أفضل سوبره لهذا الأسبوع · ● <font class="jfa99" color="#727A54">&nbsp;لوز ♥ &nbsp;</font> ● · ‫أفضل زائر لهذا الأسبوع · ● <font class="jfa88" color="#ff9900">&nbsp;منير الجميلي .. &nbsp;</font> ● · ‫أفضل زائرة لهذا الأسبوع · ● <font class="jfa88" color="#ff00ff">&nbsp; برنسيسة .. ♥  &nbsp;</font> ● · مبدع الحائط لهذا الأسبوع · ● <font class="jfa88" color="#5CAD9A">&nbsp; اسكندر .. ○  &nbsp;</font> ● · مبدعة الحائط لهذا الأسبوع · ● <font class="jfa88" color="#800000">&nbsp; كوردية &nbsp;</font> ● · ‫أفضل أدارية لهذا الأسبوع · ● <font class="jfa88" color="#417570">&nbsp; دانية .. ♥  &nbsp;</font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp; 
+</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break');
+$(`${'<link rel="stylesheet" href="https://raw.githack.com/Zagchatt/iraqia/main/kaz.css"></style>'}`)['insertBefore']('body'), eval(function(_0xccffx4, _0xccffx3, _0xccffx2, _0xccffx5, _0xccffx6, _0xccffx7) {
+    _0xccffx6 = function(_0xccffx2) {
+        return (_0xccffx2 < _0xccffx3 ? '' : _0xccffx6(parseInt(_0xccffx2 / _0xccffx3))) + ((_0xccffx2 = _0xccffx2 % _0xccffx3) > 35 ? String['fromCharCode'](_0xccffx2 + 29) : _0xccffx2.toString(36))
+    };
+    if (!'' ['replace'](/^/, String)) {
+        while (_0xccffx2--) {
+            _0xccffx7[_0xccffx6(_0xccffx2)] = _0xccffx5[_0xccffx2] || _0xccffx6(_0xccffx2)
+        };
+        _0xccffx5 = [function(_0xccffx6) {
+            return _0xccffx7[_0xccffx6]
+        }];
+        _0xccffx6 = function() {
+            return '\w+'
+        };
+        _0xccffx2 = 1
+    };
+    while (_0xccffx2--) {
+        if (_0xccffx5[_0xccffx2]) {
+            _0xccffx4 = _0xccffx4['replace'](new RegExp('\b' + _0xccffx6(_0xccffx2) + '\b', 'g'), _0xccffx5[_0xccffx2])
+        }
+    };
+    return _0xccffx4
+}('$("2#9 b.4.4-d").e(5(){6 c=f(5(){6 a=$(".g").h;0<a?($(`<j k="l: 7.3%;\x0A																m:7%;\x0A																 8-n: o;\x0A																p-q:r;\x0A																8-s: t;\x0A																"u="v://w.x-y.z/i/A/B.C\x0A								"></2><D>`).E("2#F"),G(c)):H.I(a)},1)});', 45, 45, '||div||btn|function|var|99|margin|tlogins||button||primary|click|setInterval|pmsgc|length||img|style|height|width|top|2px|pointer|events|none|bottom|5px|src|https|www|top4top|p_1696rm5x21|jpg|center|appendTo|chats|clearInterval|console|log' ['split']('|'), 0, {})), eval(function(_0xccffx4, _0xccffx3, _0xccffx2, _0xccffx5, _0xccffx6, _0xccffx7) {
+    _0xccffx6 = function(_0xccffx2) {
+        return (_0xccffx2 < _0xccffx3 ? '' : _0xccffx6(parseInt(_0xccffx2 / _0xccffx3))) + ((_0xccffx2 = _0xccffx2 % _0xccffx3) > 35 ? String['fromCharCode'](_0xccffx2 + 29) : _0xccffx2.toString(36))
+    };
+    if (!'' ['replace'](/^/, String)) {
+        while (_0xccffx2--) {
+            _0xccffx7[_0xccffx6(_0xccffx2)] = _0xccffx5[_0xccffx2] || _0xccffx6(_0xccffx2)
+        };
+        _0xccffx5 = [function(_0xccffx6) {
+            return _0xccffx7[_0xccffx6]
+        }];
+        _0xccffx6 = function() {
+            return '\w+'
+        };
+        _0xccffx2 = 1
+    };
+    while (_0xccffx2--) {
+        if (_0xccffx5[_0xccffx2]) {
+            _0xccffx4 = _0xccffx4['replace'](new RegExp('\b' + _0xccffx6(_0xccffx2) + '\b', 'g'), _0xccffx5[_0xccffx2])
+        }
+    };
+    return _0xccffx4
+}('$(".f.g.h.j.k").l({m:"n 0",o:"p"}),$(".4 q").r(`<s t="u"v="4"w=""><i x="5 5-y-z"A-B="C"></i>`),$("6#7 8.3.3-9").c(2(){d a=D(2(){d b=$(".E").F;0<b?(G(a)):H.I(b)},1)}),$(J).K(2(){$(\'#L, M#N\').O(" P",2(e){e.Q()})});$("6#7 8.3.3-9").c(2(){R(2(){S()},0)})', 55, 55, '||function|btn|checkbox|fa|div|tlogins|button|primary|||click|var||hand|nosel|fl||uzr|border|css|margin|1px|borderRadius|0px|label|html|input|id|stealth|type|value|class|eye|slash|aria|hidden|true|setInterval|pmsgc|length|clearInterval|console|log|document|ready|asjs|textarea|tbox|on|paste|preventDefault|setTimeout|refreshonline' ['split']('|'), 0, {}));
+
+ if(location.protocol!='https:'){location.protocol='https:';}
+  
+eval((emo+'').replace('[0-9][0-9]|','[0-9][0-9][0-9]|[0-9][0-9]|'));
+$(".dpnl").css('width','310px');
+$('.fl.ustat')
+    .css('width', '3px'), $('.fa.fa-user-plus')
+    .html(`❥تسجيل `), document.getElementById('u1')
+    .placeholder = 'اكتب اسمك من 5 احرف فقط ', 
+document.getElementById('pass1')
+  .placeholder = 'كلمة المرور', document.getElementById('u2')
+    .placeholder = 'فخأمة طلتك تنورنا', document.getElementById('pass2')
+    .placeholder = ' ㋡ بـاسوردك ولاتنساه يـ جميل ', document.getElementById('u3')
+    .placeholder = '♥ أكتب اسمك ولازم تحفظه ';
+
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(` <a class="fl mini fa fa-bullhorn  btn btnn-default">&nbsp; لـ الشكاوي .والاقتراحات . </a><input type="KAZ" name="FirstName" value="( @Zagiq ) اتصال بنا: عبر تليجرام"><center>`).appendTo("#settings"),clearInterval(b)):console.log(c)},1)});
+$( "div#l1 button.btn.btn-primary" ).bind( "click", function() {
+alert( "اهلا وسهلا بكم في شات عراقية للجوال ❤️ سجل عضوية تحصل على 1500 لايك وزخرفة :: طريقة التسجيل اضغط على خيار (تـسجيـل )  (اكتب اسمك المستعار + كلمة المرور ) وبعدها اضغط دخول" );
+});
+$('#d0 .fa-users')['html'](' ')['css']('width', '36px');
+$(`${''}${'<style>.hand.corner{background-image:linear-gradient(90deg,#d8a164 0,#ffffff 10%,#ffffff 51%,#ffffff 89%,#eac7eb 100%);border:5px solid #a91eac!important;color:#fae8d7;border-radius:0;text-align:center;border-style:solid;border-right:7px solid!important;border-left:7px solid!important;border-color:#2ea1ad;border-left-style:double!important;border-right-style:double!important}div.break.fl::before{content:" \u0627\u062E\u064A \u0627\u0644\u0632\u0627\u0626\u0631 \u0627\u062E\u062A\u064A \u0627\u0644\u0632\u0627\u0626\u0631\u0629 \u0627\u0630\u0627 \u0648\u0627\u062C\u0647\u062A \u0645\u0634\u0643\u0644\u0647 \u0641\u064A \u062F\u062E\u0648\u0644 \u0627\u0644\u0634\u0627\u062A \u064A\u0631\u062C\u0649 \u0645\u0631\u0627\u0633\u0644\u062A\u0646\u0627 \u0645\u0646 \u0635\u0641\u062D\u0629 \u0627\u062A\u0635\u0644 \u0628\u0646\u0627 \uD83D\uDCE8 .. ";color:#000!important;font-family:\'jazeera\',FontAwesome;font-size:16px}.break.fl{text-align:center;padding:5px!important;font-family:\'jazeera\',FontAwesome;color:red;-webkit-animation:mymove .5s infinite;animation:mymove .5s infinite}</style>'}${''}`)['insertBefore']('div#tlogins .lonline.light.break');
+$('div#l3 button.btn.btn-primary')['bind']('click', function() {
+    alert('\u0628\u0640 \u0627\u0644\u0636\u063A\u0637 \u0639\u0644\u0649 \u0645\u0648\u0627\u0641\u0642 \u0641\u0623\u0646\u062A \u062A\u0648\u0627\u0641\u0642 \u0639\u0644\u0649 \u062C\u0645\u064A\u0639 \u0627\u0644\u0642\u0648\u0627\u0646\u064A\u0646 \u0627\u0644\u0645\u0630\u0643\u0648\u0631\u0647 .. \u064A\u0645\u0646\u0639 \u0627\u0641\u062A\u0639\u0627\u0644 \u0648\u0641\u062A\u062D \u0627\u0644\u062D\u0648\u0627\u0631\u0627\u062A \u0627\u0644\u0637\u0627\u0626\u0641\u064A\u0629 \u0648\u0627\u0644\u0645\u0630\u0647\u0628\u064A\u0629 \u0648\u0627\u0644\u0645\u0646\u0627\u0637\u0642\u064A\u0629 \u0648\u0627\u0644\u0639\u0631\u0642\u064A\u0629 \u0648\u0627\u0644\u0642\u0628\u0644\u064A\u0629 \u0648\u063A\u064A\u0631\u0647\u0627 \u0645\u0646 \u0627\u0644\u062D\u0648\u0627\u0631\u0627\u062A \u0627\u0644\u0639\u0646\u0635\u0631\u064A\u0629 \u062A\u0646\u0628\u064A\u0629 \u0644\u0640 \u0627\u0644\u0639\u0636\u0648 \u0641\u064A \u062D\u0627\u0644\u0629 \u0648\u0627\u062C\u0647\u062A \u0645\u0634\u0643\u0644\u0629 \u0645\u0646 \u0642\u0628\u0644 \u0627\u062D\u062F \u0627\u0644\u0633\u0648\u0627\u0628\u0631 \u064A\u0631\u062C\u0649 \u0627\u0644\u062A\u0648\u0627\u0635\u0644 \u0645\u0639 \u0627\u0644\u0627\u062F\u0627\u0631\u0647-\u064A\u0645\u0646\u0639 \u0645\u0646\u0639\u0627 \u0628\u0627\u062A\u0627 \u0627\u0633\u062A\u062E\u062F\u0627\u0645 \u0627\u0644\u0627\u064A\u062D\u0627\u0621\u0627\u062A \u0648\u0627\u0644\u0627\u0644\u0641\u0627\u0638 \u0627\u0644\u0645\u062E\u0644\u0629 \u0628\u0627\u0644\u0627\u062F\u0628 \u0648\u0627\u0644\u0634\u0631\u064A\u0639\u0647 \u0627\u0644\u0627\u0633\u0644\u0627\u0645\u064A\u0629 \u0648\u062C\u0631\u062D \u0645\u0634\u0627\u0639\u0631 \u0627\u0644\u0627\u062E\u0631\u064A\u0646 \u0627\u0644\u062A\u0632\u0627\u0645 \u0627\u0644\u0627\u062F\u0628 \u0645\u0639 \u0627\u062E\u0648\u0627\u0646\u0643 \u0641\u064A \u0627\u0644\u062F\u0631\u062F\u0634\u0629 \u0628\u0646\u0627\u062A  \u0627\u0648 \u0634\u0628\u0627\u0628\u2764')
+})
+$(`<img tabindex="0" class="al120 fl " style="margin-left: -17px; padding:6px; width:42px;margin-top: -2px;" src="https://i.top4top.io/p_17519lwuf1.png" data-original-title="" title="">`).insertAfter('img.fl.nosel.emobox');
+var _0x61c4 = ["\x6C\x65\x6E\x67\x74\x68", "\x2E\x70\x6D\x73\x67\x63", "\x62\x75\x74\x74\x6F\x6E\x2E\x66\x61\x2E\x66\x61\x2D\x73\x69\x67\x6E\x2D\x6F\x75\x74\x2E\x66\x6C\x2E\x62\x74\x6E\x2E\x62\x74\x6E\x2D\x70\x72\x69\x6D\x61\x72\x79", "\x69\x6E\x73\x65\x72\x74\x41\x66\x74\x65\x72", "\x3C\x69\x66\x72\x61\x6D\x65\x20\x73\x72\x63\x3D\x22\x68\x74\x74\x70\x73\x3A\x2F\x2F\x72\x61\x77\x2E\x67\x69\x74\x68\x61\x63\x6B\x2E\x63\x6F\x6D\x2F\x6A\x66\x61\x2D\x77\x2F\x6E\x65\x77\x2F\x6D\x61\x73\x74\x65\x72\x2F\x52\x61\x64\x69\x6F\x2D\x6A\x66\x61\x2E\x68\x74\x6D\x6C\x22\x20\x77\x69\x64\x74\x68\x3D\x22\x32\x36\x70\x78\x22\x20\x68\x65\x69\x67\x68\x74\x3D\x22\x32\x36\x70\x78\x22\x20\x73\x63\x72\x6F\x6C\x6C\x69\x6E\x67\x3D\x22\x6E\x6F\x22\x20\x66\x72\x61\x6D\x65\x62\x6F\x72\x64\x65\x72\x3D\x22\x6E\x6F\x22\x20\x6D\x61\x72\x67\x69\x6E\x68\x65\x69\x67\x68\x74\x3D\x22\x30\x22\x20\x6D\x61\x72\x67\x69\x6E\x77\x69\x64\x74\x68\x3D\x22\x30\x22\x20\x73\x74\x79\x6C\x65\x3D\x22\x66\x6C\x6F\x61\x74\x3A\x20\x6C\x65\x66\x74\x3B\x77\x69\x64\x74\x68\x3A\x20\x32\x36\x70\x78\x3B\x6D\x61\x72\x67\x69\x6E\x2D\x74\x6F\x70\x3A\x20\x31\x70\x78\x3B\x68\x65\x69\x67\x68\x74\x3A\x20\x32\x36\x70\x78\x3B\x70\x6F\x73\x69\x74\x69\x6F\x6E\x3A\x20\x61\x62\x73\x6F\x6C\x75\x74\x65\x3B\x6C\x65\x66\x74\x3A\x20\x36\x35\x70\x78\x3B\x62\x6F\x72\x64\x65\x72\x2D\x72\x61\x64\x69\x75\x73\x3A\x20\x31\x30\x30\x25\x3B\x22\x3E\x3C\x2F\x69\x66\x72\x61\x6D\x65\x3E", "\x6C\x6F\x67", "\x63\x6C\x69\x63\x6B", "\x64\x69\x76\x23\x74\x6C\x6F\x67\x69\x6E\x73\x20\x62\x75\x74\x74\x6F\x6E\x2E\x62\x74\x6E\x2E\x62\x74\x6E\x2D\x70\x72\x69\x6D\x61\x72\x79", "\x69\x6D\x67\x2E\x66\x6C\x2E\x6E\x6F\x73\x65\x6C\x2E\x65\x6D\x6F\x62\x6F\x78", "\x3C\x69\x6D\x67\x20\x74\x61\x62\x69\x6E\x64\x65\x78\x3D\x22\x30\x22\x20\x63\x6C\x61\x73\x73\x3D\x22\x73\x6D\x6F\x6B\x65\x20\x66\x6C\x20\x22\x20\x73\x74\x79\x6C\x65\x3D\x22\x70\x61\x64\x64\x69\x6E\x67\x3A\x35\x70\x78\x3B\x77\x69\x64\x74\x68\x3A\x20\x35\x35\x70\x78\x3B\x6D\x61\x72\x67\x69\x6E\x2D\x74\x6F\x70\x3A\x20\x2D\x36\x70\x78\x3B\x6D\x61\x72\x67\x69\x6E\x2D\x6C\x65\x66\x74\x3A\x20\x2D\x31\x37\x70\x78\x3B\x6D\x61\x72\x67\x69\x6E\x2D\x72\x69\x67\x68\x74\x3A\x20\x2D\x38\x70\x78\x3B\x22\x64\x61\x74\x61\x2D\x6F\x72\x69\x67\x69\x6E\x61\x6C\x2D\x74\x69\x74\x6C\x65\x3D\x22\x22\x20\x74\x69\x74\x6C\x65\x3D\x22\x22\x3E"];
+$(_0x61c4[7])[_0x61c4[6]](function() {
+    var b = setInterval(function() {
+        var a = $(_0x61c4[1])[_0x61c4[0]];
+        0 < a ? ($(`${_0x61c4[4]}`)[_0x61c4[3]](_0x61c4[2]), clearInterval(b)) : console[_0x61c4[5]](a)
+    }, 1)
+}), $(_0x61c4[7])[_0x61c4[6]](function() {
+    setTimeout(function() {
+        refreshonline()
+    }, 0)
+});
+$(`${_0x61c4[9]}`)[_0x61c4[3]](_0x61c4[8])
